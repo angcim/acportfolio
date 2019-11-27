aca7b59b27e2530053b7a800248fab7e9b350dc1
Angelika Cimene <angelikacimene@gmail.com>
2019-11-26 19:10:41 -0500

    fix images
    no more svg

--- scripts.js
+++ scripts.js
@@ -4,64 +4,64 @@ console.log('reading js');
 // ******************** MOBILE VARIABLES ********************
 
 // PROJECTS
-var projectIcon = document.querySelector('#projectLink');
+var projectIcon = document.querySelector('#projectIcon');
 var leftArrow = document.querySelector('#leftArrow');
 var rightArrow = document.querySelector('#rightArrow');
 
 // ABOUT ME
-var aboutIcon = document.querySelector('#portraitLink');
+var aboutIcon = document.querySelector('#portraitIcon');
 var leftArrow2 = document.querySelector('#leftArrow2');
 var rightArrow2 = document.querySelector('#rightArrow2');
 
 // CONNECT
-var connectIcon = document.querySelector('#connectLink');
+var connectIcon = document.querySelector('#connectIcon');
 var leftArrow3 = document.querySelector('#leftArrow3');
 var rightArrow3 = document.querySelector('#rightArrow3');
 
 // CONTACT
-var contactIcon = document.querySelector('#contactLink');
+var contactIcon = document.querySelector('#contactIcon');
 var leftArrow4 = document.querySelector('#leftArrow4');
 var rightArrow4 = document.querySelector('#rightArrow4');
 
 
 // RIGHT ARROWS
 rightArrow.addEventListener('click', function() {
-  document.querySelector('#mobileProjects').style.display = 'none';
-  document.querySelector('#mobileAbout').style.display = 'block';
+  document.querySelector('#mobileProjectsLink').style.display = 'none';
+  document.querySelector('#mobileAboutLink').style.display = 'block';
 });
 
 rightArrow2.addEventListener('click', function() {
-  document.querySelector('#mobileAbout').style.display = 'none';
-  document.querySelector('#mobileConnect').style.display = 'block';
+  document.querySelector('#mobileAboutLink').style.display = 'none';
+  document.querySelector('#mobileConnectLink').style.display = 'block';
 });
 
 rightArrow3.addEventListener('click', function() {
-  document.querySelector('#mobileConnect').style.display = 'none';
-  document.querySelector('#mobileContact').style.display = 'block';
+  document.querySelector('#mobileConnectLink').style.display = 'none';
+  document.querySelector('#mobileContactLink').style.display = 'block';
 });
 
 rightArrow4.addEventListener('click', function() {
-  document.querySelector('#mobileContact').style.display = 'none';
-  document.querySelector('#mobileProjects').style.display = 'block';
+  document.querySelector('#mobileContactLink').style.display = 'none';
+  document.querySelector('#mobileProjectsLink').style.display = 'block';
 });
 
 // LEFT ARROWS
 leftArrow.addEventListener('click', function() {
-  document.querySelector('#mobileProjects').style.display = 'none';
-  document.querySelector('#mobileContact').style.display = 'block';
+  document.querySelector('#mobileProjectsLink').style.display = 'none';
+  document.querySelector('#mobileContactLink').style.display = 'block';
 });
 
 leftArrow2.addEventListener('click', function() {
-  document.querySelector('#mobileAbout').style.display = 'none';
-  document.querySelector('#mobileProjects').style.display = 'block';
+  document.querySelector('#mobileAboutLink').style.display = 'none';
+  document.querySelector('#mobileProjectsLink').style.display = 'block';
 });
 
 leftArrow3.addEventListener('click', function() {
-  document.querySelector('#mobileConnect').style.display = 'none';
-  document.querySelector('#mobileAbout').style.display = 'block';
+  document.querySelector('#mobileConnectLink').style.display = 'none';
+  document.querySelector('#mobileAboutLink').style.display = 'block';
 });
 
 leftArrow4.addEventListener('click', function() {
-  document.querySelector('#mobileContact').style.display = 'none';
-  document.querySelector('#mobileConnect').style.display = 'block';
+  document.querySelector('#mobileContactLink').style.display = 'none';
+  document.querySelector('#mobileConnectLink').style.display = 'block';
 });
